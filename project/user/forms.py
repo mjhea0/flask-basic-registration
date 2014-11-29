d8e72385f8ce81acb62963db35dc36dcee1f06b0
Michael Herman <hermanmu@gmail.com>
2014-11-28 19:58:03 -0600

    various updates

--- project/user/forms.py
+++ project/user/forms.py
@@ -9,20 +9,24 @@ from project.models import User
 
 
 class LoginForm(Form):
-    email = TextField('email', [DataRequired(), Email()])
-    password = PasswordField('password', [DataRequired()])
+    email = TextField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
 
 
 class RegisterForm(Form):
     email = TextField(
-        'email', [DataRequired(), Email(message=None), Length(min=6, max=40)])
+        'email',
+        validators=[DataRequired(), Email(message=None), Length(min=6, max=40)])
     password = PasswordField(
         'password',
-        [DataRequired(), Length(min=6, max=25)]
+        validators=[DataRequired(), Length(min=6, max=25)]
     )
     confirm = PasswordField(
         'Repeat password',
-        [DataRequired(), EqualTo('password', message='Passwords must match.')]
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords must match.')
+        ]
     )
 
     def validate(self):
@@ -34,3 +38,17 @@ class RegisterForm(Form):
             self.email.errors.append("Email already registered")
             return False
         return True
+
+
+class ChangePasswordForm(Form):
+    password = PasswordField(
+        'password',
+        validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    confirm = PasswordField(
+        'Repeat password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords must match.')
+        ]
+    )
