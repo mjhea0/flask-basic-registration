0adcf6d05ff33c0cbefbe715c7e4fe1c7f7c451c
Michael Herman <hermanmu@gmail.com>
2015-03-13 04:56:43 -0600

    ported to python3

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,19 +9,6 @@ from flask.ext.testing import TestCase
 from project import app
 
 
-class TestDevelopmentConfig(TestCase):
-
-    def create_app(self):
-        app.config.from_object('project.config.DevelopmentConfig')
-        return app
-
-    def test_app_is_development(self):
-        self.assertFalse(current_app.config['TESTING'])
-        self.assertTrue(app.config['DEBUG'] is True)
-        self.assertTrue(app.config['WTF_CSRF_ENABLED'] is False)
-        self.assertTrue(app.config['DEBUG_TB_ENABLED'] is True)
-
-
 class TestTestingConfig(TestCase):
 
     def create_app(self):
@@ -34,19 +21,8 @@ class TestTestingConfig(TestCase):
         self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 1)
         self.assertTrue(app.config['WTF_CSRF_ENABLED'] is False)
 
-
-class TestProductionConfig(TestCase):
-
-    def create_app(self):
-        app.config.from_object('project.config.ProductionConfig')
-        return app
-
-    def test_app_is_production(self):
-        self.assertFalse(current_app.config['TESTING'])
-        self.assertTrue(app.config['DEBUG'] is False)
-        self.assertTrue(app.config['DEBUG_TB_ENABLED'] is False)
-        self.assertTrue(app.config['WTF_CSRF_ENABLED'] is True)
-        self.assertTrue(app.config['BCRYPT_LOG_ROUNDS'] == 13)
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
 
 
 if __name__ == '__main__':
